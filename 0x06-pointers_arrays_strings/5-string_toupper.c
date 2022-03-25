03a700a10d6e5f0e674a5dca7ab3e0e0c6bb2df5
jaba <jabajnan8@gmail.com>
2022-03-26 00:05:26 +0300

    edited change to lower case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * string_toupper - change all lowercase to uppercase
- * @n: pointer
- *
- * Return: n
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *string_toupper(char *n)
-{
-	int i;
 
-	i = 0;
-	while (n[i] != '\0')
-	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
-		i++;
-	}
-	return (n);
+char *string_toupper(char *s)
+{
+  int count = 0;
+
+  while (*(s + count) != '\0')
+  {
+    if ((*(s + count) >= 97) && (*(s + count) <= 122))
+      *(s + count) = *(s + count) - 32;
+    count++;
+  }
+
+  return (s);
 }
